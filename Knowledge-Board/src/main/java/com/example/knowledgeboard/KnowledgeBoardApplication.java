f0ebb414489863acfebce712f8c220e9b3025600
alsdl0629 <alsdl0629@dsm.hs.kr>
2022-07-26 23:57:05 +0900

    🌟 :: KnowledgeBoard

---
+++ Knowledge-Board/src/main/java/com/example/knowledgeboard/KnowledgeBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.knowledgeboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowledgeBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KnowledgeBoardApplication.class, args);
+    }
+
+}
---
+++ Knowledge-Board/src/test/java/com/example/knowledgeboard/KnowledgeBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.knowledgeboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowledgeBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
