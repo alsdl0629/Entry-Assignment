0a1c1f6fd5d6f6f4cd175df091daa02beff53832
alsdl0629 <alsdl0629@dsm.hs.kr>
2022-08-02 17:57:50 +0900

    ADD::MessageResponse

---
+++ Knowledge-Board/src/main/java/com/example/knowledgeboard/dto/MessageResponse.java
@@ -0,0 +1,14 @@
+package com.example.knowledgeboard.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+@Builder
+public class MessageResponse {
+
+    private String message;
+
+}
