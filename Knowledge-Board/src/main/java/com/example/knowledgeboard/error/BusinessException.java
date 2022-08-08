ff118dca30db7fc9e69026e8ce201bc35e9f393d
alsdl0629 <alsdl0629@dsm.hs.kr>
2022-08-08 22:53:35 +0900

    UPDATE::BusinessException

--- Knowledge-Board/src/main/java/com/example/knowledgeboard/error/BusinessException.java
+++ Knowledge-Board/src/main/java/com/example/knowledgeboard/error/BusinessException.java
@@ -1,13 +1,12 @@
 package com.example.knowledgeboard.error;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
+@RequiredArgsConstructor
 public class BusinessException extends RuntimeException {
+
     private final ErrorCode errorCode;
 
-    public BusinessException(ErrorCode errorCode) {
-        super(errorCode.getMessage());
-        this.errorCode = errorCode;
-    }
 }
