1049c1028ec1d0a6de4eb8a7ab56033aca344627
alsdl0629 <alsdl0629@dsm.hs.kr>
2022-08-09 23:17:23 +0900

    ADD::BoardEntity

---
+++ Knowledge-Board/src/main/java/com/example/knowledgeboard/entity/board/Board.java
@@ -0,0 +1,41 @@
+package com.example.knowledgeboard.entity.board;
+
+import com.example.knowledgeboard.entity.BaseTimeEntity;
+import com.example.knowledgeboard.entity.user.User;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+@Entity
+public class Board extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+    private Integer views;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public void update(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public void addViews() {
+        this.views += 1;
+    }
+
+}
