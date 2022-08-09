a94bd51589b293775a4c89f3b76bb8bad8fd0854
alsdl0629 <alsdl0629@dsm.hs.kr>
2022-08-09 23:18:50 +0900

    ADD::CreateFeedRequest

---
+++ Knowledge-Board/src/main/java/com/example/knowledgeboard/dto/board/request/CreateFeedRequest.java
@@ -0,0 +1,21 @@
+package com.example.knowledgeboard.dto.board.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Getter
+@AllArgsConstructor
+public class CreateFeedRequest {
+
+    @NotBlank(message = "title은 필수 항목입니다.")
+    @Size(max = 30, message = "title은 30글자 이하여야 합니다.")
+    private String title;
+
+    @NotBlank(message = "content는 필수 항목입니다.")
+    @Size(max = 500, message = "content는 500글자 이하여야 합니다.")
+    private String content;
+
+}
