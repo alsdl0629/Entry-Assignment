cd4b5a88be946d89b0c18bd20d0340ecd3868857
alsdl0629 <alsdl0629@dsm.hs.kr>
2022-08-09 23:16:01 +0900

    ADD::ExpiredTokenException

--- Knowledge-Board/src/main/java/com/example/knowledgeboard/error/ErrorCode.java
+++ Knowledge-Board/src/main/java/com/example/knowledgeboard/error/ErrorCode.java
@@ -7,11 +7,17 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ErrorCode {
 
-    //회원
+
+    INVALID_TOKEN(401, "Invalid Token"),
+    EXPIRED_TOKEN(401, "Expired_Token"),
+
     INVALID_PASSWORD(401, "Invalid Password"),
+    FORBIDDEN(403, "Forbidden"),
     USER_NOT_FOUND(404, "User Not Found"),
     AUTHENTICATION_NOT_FOUND(404, "Authentication Not Found"),
-    USER_ALREADY_EXISTS(409, "User Already Exists");
+    USER_ALREADY_EXISTS(409, "User Already Exists"),
+
+    BOARD_NOT_FOUND(404, "Board Not Found");
 
 
     private final int status;
