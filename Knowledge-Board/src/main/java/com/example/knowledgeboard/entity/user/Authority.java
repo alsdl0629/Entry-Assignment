928c35072f1a1c353e6e4642ee35ff1192e77c18
alsdl0629 <alsdl0629@dsm.hs.kr>
2022-08-10 22:31:04 +0900

    ADD::Authority

---
+++ Knowledge-Board/src/main/java/com/example/knowledgeboard/entity/user/Authority.java
@@ -0,0 +1,5 @@
+package com.example.knowledgeboard.entity.user;
+
+public enum Authority {
+    ROLE_USER, ROLE_ADMIN
+}
--- Knowledge-Board/src/main/java/com/example/knowledgeboard/security/auth/AuthDetails.java
+++ Knowledge-Board/src/main/java/com/example/knowledgeboard/security/auth/AuthDetails.java
@@ -4,6 +4,7 @@ import com.example.knowledgeboard.entity.user.User;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
@@ -17,7 +18,7 @@ public class AuthDetails implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return Collections.singletonList(new SimpleGrantedAuthority(user.getAuthority().name()));
     }
 
     @Override
