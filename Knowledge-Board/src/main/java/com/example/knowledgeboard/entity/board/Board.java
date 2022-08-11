ca39a21f74a9afc2cbb0c0b85d907429bca02fd3
alsdl0629 <alsdl0629@dsm.hs.kr>
2022-08-12 00:45:51 +0900

    UPDATE::Board

--- Knowledge-Board/src/main/java/com/example/knowledgeboard/entity/board/Board.java
+++ Knowledge-Board/src/main/java/com/example/knowledgeboard/entity/board/Board.java
@@ -1,11 +1,11 @@
 package com.example.knowledgeboard.entity.board;
 
-import com.example.knowledgeboard.entity.BaseTimeEntity;
 import com.example.knowledgeboard.entity.like.Like;
 import com.example.knowledgeboard.entity.user.User;
 import lombok.*;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
@@ -13,7 +13,7 @@ import java.util.List;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Builder
 @Entity
-public class Board extends BaseTimeEntity {
+public class Board {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -25,19 +25,37 @@ public class Board extends BaseTimeEntity {
     @Column(nullable = false)
     private String content;
 
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
     private Integer views;
+
+    private Integer likeCounts;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
-    
+
+    @OneToMany(mappedBy = "board", cascade = CascadeType.REMOVE)
+    private List<Like> likes;
+
     public void updateFeed(String title, String content) {
         this.title = title;
         this.content = content;
+        this.createdAt = LocalDateTime.now();
     }
 
     public void addViews() {
         this.views += 1;
+    }
+
+    public void addLikeCount() {
+        this.likeCounts += 1;
+    }
+
+    public void minusLikeCount() {
+        this.likeCounts -= 1;
     }
 
 }
