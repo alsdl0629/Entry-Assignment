c8b82bd3d04ddc241ec522178fa0d461757eb4ce
alsdl0629 <alsdl0629@dsm.hs.kr>
2022-08-14 22:19:08 +0900

    ADD::UpdateReplyRequest

---
+++ Knowledge-Board/src/main/java/com/example/knowledgeboard/domain/reply/api/dto/request/UpdateReplyRequest.java
@@ -0,0 +1,17 @@
+package com.example.knowledgeboard.domain.reply.api.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Getter
+@NoArgsConstructor
+public class UpdateReplyRequest {
+
+    @NotBlank(message = "content는 필수 항목입니다.")
+    @Size(max = 250, message = "content는 250글자 이하여야 합니다.")
+    private String content;
+
+}
